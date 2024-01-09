bfa4c4847248095e9530ea5c1d001f2587a33391
Alex Martishin <asmartishin@gmail.com>
2024-01-09 10:37:42 +0100

    Ktlint fix

--- common/src/main/kotlin/com/example/common/models/Extensions.kt
+++ common/src/main/kotlin/com/example/common/models/Extensions.kt
@@ -2,7 +2,6 @@ package com.example.common.models
 
 import com.example.common.models.domain.RandomNumber
 import com.example.common.models.errors.DomainError
-
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 
@@ -10,5 +9,4 @@ fun DomainError.toResponse() =
     ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
         .body(mapOf("message" to this.reason, "thread" to Thread.currentThread().toString()))
 
-fun RandomNumber.toResponse() =
-    ResponseEntity.ok(mapOf("number" to this.value, "thread" to Thread.currentThread().toString()))
+fun RandomNumber.toResponse() = ResponseEntity.ok(mapOf("number" to this.value, "thread" to Thread.currentThread().toString()))
